b63d17ec9d1c761f4d3767eb3589c08abceccf81
Ella2401 <70705614+Ella2401@users.noreply.github.com>
2021-01-30 00:26:58 +0400

    Create main.java

---
+++ main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import menu.Menu;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Menu m = new Menu();
+        m.displayMenu();
+    }
+}
